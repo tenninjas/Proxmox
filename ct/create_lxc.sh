0dc36c9ac085f8af7b0d3bf0c852c1bd3a8f20ba
tenninjas <tenninjas>
2024-09-04 06:35:38 +0800

    debug

--- ct/create_lxc.sh
+++ ct/create_lxc.sh
@@ -181,6 +181,7 @@ PCT_OPTIONS=(${PCT_OPTIONS[@]:-${DEFAULT_PCT_OPTIONS[@]}})
 
 # Create container
 msg_info "Creating LXC Container"
+msg_info "[Debug] pct create $CTID ${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE} ${PCT_OPTIONS[@]}"
 pct create $CTID ${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE} ${PCT_OPTIONS[@]} >/dev/null ||
   exit "A problem occured while trying to create container."
 msg_ok "LXC Container ${BL}$CTID${CL} ${GN}was successfully created."
